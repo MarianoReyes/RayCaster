ff1ce46b09b72d752af0849bfeb7f7d1e568d9a8
MarianoReyes <josemarianoreyesh02@gmail.com>
2022-11-24 09:20:54 -0600

    lab4 terminado

--- lab4.py
+++ lab4.py
@@ -13,7 +13,7 @@ screen = pygame.display.set_mode(
     pygame.OPENGL | pygame.DOUBLEBUF
 )
 
-model = Obj('./cajaybarril.obj')
+model = Obj('./mask.obj')
 
 vertex_shader = """
 #version 460
@@ -33,64 +33,249 @@ void main()
 }
 """
 
+# shaders
+
 fragment_shader = """
 #version 460
 layout (location = 0) out vec4 fragColor;
 
 uniform vec3 color;
-
 in vec3 ourColor;
 
 void main()
 {
-    //fragColor = vec4(color, 1.0f);
-    fragColor = vec4(ourColor, 1.0f);
+    //fragColor = vec4(ourColor, 1.0f);
+    fragColor = vec4(color, 1.0f);
+
 }
 """
 
 fragment_shader2 = """
 #version 460
+
+vec2 iResolution = vec2(2, 2);
 layout (location = 0) out vec4 fragColor;
+in vec2 fragCoord;
+uniform float iTime;
+ 
+mat2 move(in float a) {
+    float c = cos(a);
+    float s = sin(a);
+    return mat2(c, -s, s, -c);
+}
 
-uniform vec3 color;
-in vec3 ourColor;
+float map(in vec3 st) {
+    st.xy *= move(iTime * 0.3);
+    st.xz *= move(iTime * 0.5);
+    vec3 p = st * 2.0 + iTime;
+    return length(st + vec3(sin(iTime * 0.5))) + sin(p.x + sin(p.y + sin(p.z))) * 0.5 - 2.0;
+}
 
-void main()
-{
-    //fragColor = vec4(ourColor, 1.0f);
-    fragColor = vec4(color, 1.0f);
+void main() {
+    vec2 st = fragCoord / iResolution.xy - vec2(1.0, 0.5);
 
+    vec3 col = vec3(0.0);
+    float dist = 2.5;
+
+    for (int i = 0; i <= 4; i++) {
+        vec3 st = vec3(0.0, 0.0, 1.0) + normalize(vec3(st, -1.0)) * dist;
+        float rz = map(st);
+        float f = clamp((rz - map(st + 0.1)) * 0.5, -0.5, 1.0);
+        vec3 l = vec3(0.1, 0.3, 0.4) + vec3(4.0, 2.5, 2.5) * f;
+        col = col * l + smoothstep(5.0, 2.0, rz) * 0.4 * l;
+        dist += min(rz, iTime);
+    }
+
+    fragColor = vec4(col,1.0);
 }
 """
 
 fragment_shader3 = """
 #version 460
+
+precision highp float;
+vec2 iResolution = vec2(2, 2);
 layout (location = 0) out vec4 fragColor;
+in vec2 fragCoord;
+uniform float iTime;
 
-uniform vec3 color;
-in vec3 ourColor;
 
-void main()
-{
-    //fragColor = vec4(ourColor, 1.0f);
-    fragColor = vec4(color, 1.0f);
+mat2 rot(float a) {
+    float c = cos(a), s = sin(a);
+    return mat2(c,s,-s,c);
+}
 
+const float pi = acos(-1.0);
+const float pi2 = pi*2.0;
+
+vec2 pmod(vec2 p, float r) {
+    float a = atan(p.x, p.y) + pi/r;
+    float n = pi2 / r;
+    a = floor(a/n)*n;
+    return p*rot(-a);
+}
+
+float box( vec3 p, vec3 b ) {
+    vec3 d = abs(p) - b;
+    return min(max(d.x,max(d.y,d.z)),0.0) + length(max(d,0.0));
+}
+
+float ifsBox(vec3 p) {
+    for (int i=0; i<5; i++) {
+        p = abs(p) - 1.0;
+        p.xy *= rot(iTime*0.3);
+        p.xz *= rot(iTime*0.1);
+    }
+    p.xz *= rot(iTime);
+    return box(p, vec3(0.4,0.8,0.3));
+}
+
+float map(vec3 p, vec3 cPos) {
+    vec3 p1 = p;
+    p1.x = mod(p1.x-5., 10.) - 5.;
+    p1.y = mod(p1.y-5., 10.) - 5.;
+    p1.z = mod(p1.z, 16.)-8.;
+    p1.xy = pmod(p1.xy, 5.0);
+    return ifsBox(p1);
+}
+
+void main() {
+    vec2 p = (fragCoord.xy * 2.0 - iResolution.xy) / min(iResolution.x, iResolution.y);
+
+    vec3 cPos = vec3(0.0,0.0, -3.0 * iTime);
+    // vec3 cPos = vec3(0.3*sin(iTime*0.8), 0.4*cos(iTime*0.3), -6.0 * iTime);
+    vec3 cDir = normalize(vec3(0.0, 0.0, -1.0));
+    vec3 cUp  = vec3(sin(iTime), 1.0, 0.0);
+    vec3 cSide = cross(cDir, cUp);
+
+    vec3 ray = normalize(cSide * p.x + cUp * p.y + cDir);
+
+    // Phantom Mode https://www.shadertoy.com/view/MtScWW by aiekick
+    float acc = 0.0;
+    float acc2 = 0.0;
+    float t = 0.0;
+    for (int i = 0; i < 99; i++) {
+        vec3 pos = cPos + ray * t;
+        float dist = map(pos, cPos);
+        dist = max(abs(dist), 0.02);
+        float a = exp(-dist*3.0);
+        if (mod(length(pos)+24.0*iTime, 30.0) < 3.0) {
+            a *= 2.0;
+            acc2 += a;
+        }
+        acc += a;
+        t += dist * 0.5;
+    }
+
+    vec3 col = vec3(acc * 0.01, acc * 0.011 + acc2*0.002, acc * 0.012+ acc2*0.005);
+    fragColor = vec4(col, 1.0 - t * 0.03);
 }
 """
 
 fragment_shader4 = """
 #version 460
+
+vec2 iResolution = vec2(2, 2);
 layout (location = 0) out vec4 fragColor;
+in vec2 fragCoord;
+uniform float iTime;
+float gTime;
 
-uniform vec3 color;
-in vec3 ourColor;
-
-void main()
-{
-    //fragColor = vec4(ourColor, 1.0f);
-    fragColor = vec4(color, 1.0f);
-
+mat2 rot(float a) {
+	float c = cos(a), s = sin(a);
+	return mat2(c,s,-s,c);
 }
+
+float sdBox( vec3 p, vec3 b )
+{
+	vec3 q = abs(p) - b;
+	return length(max(q,0.0)) + min(max(q.x,max(q.y,q.z)),0.0);
+}
+
+float box(vec3 pos, float scale) {
+	pos *= scale;
+	float base = sdBox(pos, vec3(.4,.4,.1)) /1.5;
+	pos.xy *= 5.;
+	pos.y -= 3.5;
+	pos.xy *= rot(.75);
+	float result = -base;
+	return result;
+}
+
+float box_set(vec3 pos, float iTime) {
+	vec3 pos_origin = pos;
+	pos = pos_origin;
+	pos .y += sin(gTime * 0.4) * 2.5;
+	pos.xy *=   rot(.8);
+	float box1 = box(pos,2. - abs(sin(gTime * 0.4)) * 1.5);
+	pos = pos_origin;
+	pos .y -=sin(gTime * 0.4) * 2.5;
+	pos.xy *=   rot(.8);
+	float box2 = box(pos,2. - abs(sin(gTime * 0.4)) * 1.5);
+	pos = pos_origin;
+	pos .x +=sin(gTime * 0.4) * 2.5;
+	pos.xy *=   rot(.8);
+	float box3 = box(pos,2. - abs(sin(gTime * 0.4)) * 1.5);	
+	pos = pos_origin;
+	pos .x -=sin(gTime * 0.4) * 2.5;
+	pos.xy *=   rot(.8);
+	float box4 = box(pos,2. - abs(sin(gTime * 0.4)) * 1.5);	
+	pos = pos_origin;
+	pos.xy *=   rot(.8);
+	float box5 = box(pos,.5) * 6.;	
+	pos = pos_origin;
+	float box6 = box(pos,.5) * 6.;	
+	float result = max(max(max(max(max(box1,box2),box3),box4),box5),box6);
+	return result;
+}
+
+float map(vec3 pos, float iTime) {
+	vec3 pos_origin = pos;
+	float box_set1 = box_set(pos, iTime);
+
+	return box_set1;
+}
+
+
+void main() {
+	vec2 p = (fragCoord.xy * 2. - iResolution.xy) / min(iResolution.x, iResolution.y);
+	vec3 ro = vec3(0., -0.2 ,iTime * 4.);
+	vec3 ray = normalize(vec3(p, 1.5));
+	ray.xy = ray.xy * rot(sin(iTime * .03) * 5.);
+	ray.yz = ray.yz * rot(sin(iTime * .05) * .2);
+	float t = 0.1;
+	vec3 col = vec3(0.);
+	float ac = 0.0;
+
+
+	for (int i = 0; i < 99; i++){
+		vec3 pos = ro + ray * t;
+		pos = mod(pos-2., 4.) -2.;
+		gTime = iTime -float(i) * 0.01;
+		
+		float d = map(pos, iTime);
+
+		d = max(abs(d), 0.01);
+		ac += exp(-d*23.);
+
+		t += d* 0.55;
+	}
+
+	col = vec3(ac * 0.02);
+
+	col +=vec3(0.,0.2 * abs(sin(iTime)),0.5 + sin(iTime) * 0.2);
+
+
+	fragColor = vec4(col ,1.0 - t * (0.02 + 0.02 * sin (iTime)));
+}
+
+/** SHADERDATA
+{
+	"title": "Octgrams",
+	"description": "Lorem ipsum dolor",
+	"model": "person"
+}
+*/
 """
 
 compiled_vertex_shader = compileShader(vertex_shader, GL_VERTEX_SHADER)
@@ -98,6 +283,8 @@ compiled_fragment_shader = compileShader(fragment_shader, GL_FRAGMENT_SHADER)
 compiled_fragment_shader2 = compileShader(fragment_shader2, GL_FRAGMENT_SHADER)
 compiled_fragment_shader3 = compileShader(fragment_shader3, GL_FRAGMENT_SHADER)
 compiled_fragment_shader4 = compileShader(fragment_shader4, GL_FRAGMENT_SHADER)
+
+# compilar shaders
 
 shader = compileProgram(
     compiled_vertex_shader,
@@ -119,8 +306,10 @@ shader4 = compileProgram(
     compiled_fragment_shader4
 )
 
-glUseProgram(shader2)
+glUseProgram(shader)
 
+
+# a partir de aca es logica para cargar objetos
 vertex = []
 for ver in model.vertices:
     for v in ver:
@@ -169,9 +358,9 @@ glBufferData(GL_ELEMENT_ARRAY_BUFFER, faces_data.nbytes,
 def calculateMatrix(angle, vector):
 
     i = glm.mat4(1)
-    translate = glm.translate(i, glm.vec3(0, 0, -5))
+    translate = glm.translate(i, glm.vec3(0, -0, 0))
     rotate = glm.rotate(i, glm.radians(angle), vector)
-    scale = glm.scale(i, glm.vec3(1, 1, 1))
+    scale = glm.scale(i, glm.vec3(13, 13, 13))
 
     """ translate = glm.translate(i, glm.vec3(0, -1, -15))
     rotate = glm.rotate(i, glm.radians(angle), vector)
@@ -204,25 +393,27 @@ def calculateMatrix(angle, vector):
     )
 
 
-running = True
+# se corre el programa
 
-glClearColor(0.0, 1.0, 1.0, 1.0)
+glClearColor(0.0, 0.0, 0.0, 1.0)
 r = 0
-changeShader = False
-shaderIndex = 0
-shaders = [shader2, shader3, shader4]
-currentShader = shader2
+cambiar = False
+numero_shader = 0
+shaders = [shader, shader2, shader3, shader4]
+shader_actual = shader
 vector = glm.vec3(0, 1, 0)
 
 prev_time = pygame.time.get_ticks()
 
+running = True
 while running:
     glClear(GL_COLOR_BUFFER_BIT)
 
-    if changeShader:
-        glUseProgram(shaders[shaderIndex])
-        currentShader = shaders[shaderIndex]
-        changeShader = False
+    # con esto se cambia de textura
+    if cambiar == True:
+        glUseProgram(shaders[numero_shader])
+        shader_actual = shaders[numero_shader]
+        cambiar = False
 
     color1 = random.random()
     color2 = random.random()
@@ -230,7 +421,7 @@ while running:
 
     color = glm.vec3(color1, color2, color3)
     glUniform3fv(
-        glGetUniformLocation(currentShader, "color"),
+        glGetUniformLocation(shader_actual, "color"),
         1,
         glm.value_ptr(color)
     )
@@ -238,7 +429,7 @@ while running:
     time = (pygame.time.get_ticks() - prev_time) / 1000
     #time = pygame.time.get_ticks()
     glUniform1f(
-        glGetUniformLocation(currentShader, "time"),
+        glGetUniformLocation(shader_actual, "time"),
         time
     )
 
@@ -252,15 +443,18 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_0:
+                numero_shader = 0
+                cambiar = True
             if event.key == pygame.K_1:
-                shaderIndex = 0
-                changeShader = True
+                numero_shader = 1
+                cambiar = True
             if event.key == pygame.K_2:
-                shaderIndex = 1
-                changeShader = True
+                numero_shader = 2
+                cambiar = True
             if event.key == pygame.K_3:
-                shaderIndex = 2
-                changeShader = True
+                numero_shader = 3
+                cambiar = True
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_RIGHT]:
@@ -268,10 +462,4 @@ while running:
         r += 0.5
     if keys[pygame.K_LEFT]:
         vector = glm.vec3(0, 1, 0)
-        r -= 0.5
-    if keys[pygame.K_UP]:
-        vector = glm.vec3(1, 0, 0)
-        r += 0.5
-    if keys[pygame.K_DOWN]:
-        vector = glm.vec3(1, 0, 0)
         r -= 0.5
